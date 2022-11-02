7c63bd22ca92524c4bbc160219e4e47b32455d07
Wayne <waynegabriel13@gmail.com>
2022-11-02 17:32:49 -0300

    code changes

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+document.querySelector('#container').innerHTML = `
+<div class="line"></div>
+    <div class="details">
+        <h1>Barbecue Meter</h1>
+        <input type="number" name="kids" id="kids" placeholder="Kids">
+        <input type="number" name="adults" id="adults" placeholder="Adults">
+        <input type="number" name="duration" id="duration" placeholder="Duration(hours)">
+        <br>
+        <input onclick="calc()" type="submit" value="Calculate">
+        <div id="result"></div>
+    </div>
+
+`
+
+
 // Getting Elements
 
 let inputAdults = document.getElementById('adults')
@@ -5,22 +20,6 @@ let inputKids = document.getElementById('kids')
 let inputDuration= document.getElementById('duration')
 let result = document.getElementById('result')
 
-
-// Calculation function
-
-function calc(){
-    console.log('calculating...')
-    let adults = inputAdults.value
-    let kids = inputKids.value
-    let duration = inputDuration.value
-
-    let totalMeat = meat(duration) * adults + (meat(duration)/2 * kids)
-    let totalBeer = beer(duration) * adults
-    let totalSoda = soda(duration) * adults + (soda(duration)/2 * kids)
-    result.innerHTML = `<p>${totalMeat / 1000}kg of meat</p>`
-    result.innerHTML += `<p>${Math.ceil(totalBeer / 355)} beer cans </p>`
-    result.innerHTML += `<p>${Math.ceil(totalSoda/2000)} soda bottles</p>`
-}
 
 // Basic quantity logic
 
@@ -45,4 +44,21 @@ function soda(duration){
         return 1000;
     }
 }
+
+// Calculation function
+
+function calc(){
+    console.log('calculating...')
+    let adults = inputAdults.value
+    let kids = inputKids.value
+    let duration = inputDuration.value
+
+    let totalMeat = meat(duration) * adults + (meat(duration)/2 * kids)
+    let totalBeer = beer(duration) * adults
+    let totalSoda = soda(duration) * adults + (soda(duration)/2 * kids)
+    result.innerHTML = `<p>${totalMeat / 1000}kg of meat</p>`
+    result.innerHTML += `<p>${Math.ceil(totalBeer / 355)} beer cans </p>`
+    result.innerHTML += `<p>${Math.ceil(totalSoda/2000)} soda bottles</p>`
+}
+
 
