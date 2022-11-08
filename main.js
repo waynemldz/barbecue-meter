5ac153c6a2d1db91ce85fec31c4d9bcf602e220a
Wayne <waynegabriel13@gmail.com>
2022-11-08 19:31:49 -0300

    upgrades: new appereance, dynamic changes in css and new logic js

--- main.js
+++ main.js
@@ -1,64 +1,71 @@
-document.querySelector('#container').innerHTML = `
-<div class="line"></div>
-    <div class="details">
-        <h1>Barbecue Meter</h1>
-        <input type="number" name="kids" id="kids" placeholder="Kids">
-        <input type="number" name="adults" id="adults" placeholder="Adults">
-        <input type="number" name="duration" id="duration" placeholder="Duration(hours)">
-        <br>
-        <input onclick="calc()" type="submit" value="Calculate">
-        <div id="result"></div>
-    </div>
-
-`
-
 
 // Getting Elements
 
 let inputAdults = document.getElementById('adults')
 let inputKids = document.getElementById('kids')
-let inputDuration= document.getElementById('duration')
+let inputDuration = document.getElementById('duration')
 let result = document.getElementById('result')
 
 
-// Basic quantity logic
+// Logic
 
-function meat(duration){
-    if(duration >= 6){
-        return 700;
-    }else{
-        return 400;
+function meat(hour) {
+    let meat = 300;
+    if (hour > 1) {
+        for (i = 1; i <= hour; i++) {
+            meat += 80;
+        }
+        return meat
+    } else {
+        return meat;
     }
 }
-function beer(duration){
-    if(duration >= 6){
-        return 3000;
-    }else{
-        return 1500;
+
+function beer(hour) {
+    let beer = 600;
+    if (hour > 1) {
+        for (i = 1; i <= hour; i++) {
+            beer += 200;
+        }
+        return beer
+    } else {
+        return beer;
     }
 }
-function soda(duration){
-    if(duration >= 6){
-        return 1500;
-    }else{
-        return 1000;
+
+function soda(hour) {
+    let soda = 500;
+    if (hour > 1) {
+        for (i = 1; i <= hour; i++) {
+            soda += 150;
+        }
+        return soda;
+    } else {
+        return soda;
     }
 }
+
 
 // Calculation function
 
-function calc(){
+function calc() {
     console.log('calculating...')
     let adults = inputAdults.value
     let kids = inputKids.value
     let duration = inputDuration.value
 
-    let totalMeat = meat(duration) * adults + (meat(duration)/2 * kids)
+
+
+    let totalMeat = meat(duration) * adults + (meat(duration) / 2 * kids)
     let totalBeer = beer(duration) * adults
     let totalSoda = soda(duration) * adults + (soda(duration)/2 * kids)
+
+
     result.innerHTML = `<p>${totalMeat / 1000}kg of meat</p>`
     result.innerHTML += `<p>${Math.ceil(totalBeer / 355)} beer cans </p>`
-    result.innerHTML += `<p>${Math.ceil(totalSoda/2000)} soda bottles</p>`
+    result.innerHTML += `<p>${Math.ceil(totalSoda / 2000)} soda bottles</p>`
+    result.style.fontFamily = 'Arial'
+    result.style.fontWeight = '600'
 }
 
 
